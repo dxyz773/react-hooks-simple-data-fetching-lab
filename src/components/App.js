1e9e19c7685a212a7c3eb34315814e3a5783e3ea
demitryedwards <dxyz773@gmail.com>
2023-05-08 09:04:30 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import { useEffect, useState } from "react";
+
+const url = "https://dog.ceo/api/breeds/image/random";
+
+function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((image) => setImage(image.message));
+  }, []);
+
+  if (!image) {
+    return <h2>Loading...</h2>;
+  }
+  return <img src={image} alt="A Random Dog"></img>;
+}
+
+export default App;
